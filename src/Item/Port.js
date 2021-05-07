42909bf13610b69bac4e3b4d671a91539bbd7e2b
rixshiki <cheunjitnice@gmail.com>
2021-05-07 09:40:19 +0700

    Port edited

--- src/Item/Port.js
+++ src/Item/Port.js
@@ -11,14 +11,18 @@ class Port extends React.Component {
 						<img src="/nice-resume/ref/smeEx.png" alt=""/>
 						<span className="center-end">SME Management project</span>
 					</a>
+					<a href="https://covid-19statedfa.netlify.app" target="_blank" className="Item-container">
+						<img src="/nice-resume/ref/Covid-19PhaseChecking.png" alt=""/>
+						<span className="center-end">Covid-19 phase checking</span>
+					</a>
+					<div className="Item-container center">
+						Pet blood bank report
+					</div>
 					<div className="Item-container">
 					<a href="/nice-resume/ref/kmitlUIDesign.pdf" target="_blank" className="Item-container">
 						<img src="/nice-resume/ref/kmitl-event-admin.jpg" alt=""/>
 						<span className="center-end">KMITL EVENT UI design</span>
 					</a>
-					</div>
-					<div className="Item-container center">
-						Pet blood bank report
 					</div>
 				</div>
 			</div>
